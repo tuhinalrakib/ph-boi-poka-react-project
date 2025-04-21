590773bd4c54584d17b364f8e39177d04481fcff
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2025-04-21 12:03:16 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+
+
+export const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <h1>Heeloo world!!!</h1>
+    }
+])
